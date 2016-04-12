d46f625ff384c2cda53f7b0fbac2fd7363acd414
Adam Bertalan <takeow.adam@gmail.com>
2016-04-12 13:26:19 +0200

    Licensz generálva, package-info.java-k hozzáadva, üres create.sql létrehozása, Logolás beizzítása

--- penzkezelo-db/src/test/java/hu/bertalanadam/prt/beadando/db/test/TestConnection.java
+++ penzkezelo-db/src/test/java/hu/bertalanadam/prt/beadando/db/test/TestConnection.java
@@ -1,7 +1,9 @@
 package hu.bertalanadam.prt.beadando.db.test;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,19 +16,23 @@ import hu.bertalanadam.prt.beadando.db.tarolo.FelhasznaloTarolo;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/spring-db.xml")
 @Transactional
-@Rollback(false)
+@Rollback(true)
 public class TestConnection {
+	
+	private static Logger logger = Logger.getLogger(TestConnection.class.getName());
 
 	@Autowired
 	FelhasznaloTarolo fhtarolo;
 
 	@Test
-	public void testInsert() {
+	public void tesztBeszurasAFelhasznaloba() {
+		logger.info("TESTLOG");
 		Felhasznalo random = new Felhasznalo();
 
-		random.setId(1L);
+		random.setFelasznalonev("Magic");
 
 		fhtarolo.save(random);
+
 	}
 
 }
